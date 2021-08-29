6a38a7ea331bac4149bc00278662fd0819c6166c
myb12 <mohammadyasinbappy@outlook.com>
2021-08-29 16:40:48 +0600

    Implemented sign up with email

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import SignIn from "../../components/sign-in/sign-in.component";
-import './sign-in-and-sign-up.styles.scss';
+import SignUp from "../../components/sign-up/sign-up.component";
+import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUpPage = () => {
-    return (
-        <div className="sign-in-sign-and-up">
-            <SignIn/>
-        </div>
-    )
-}
+  return (
+    <div className="sign-in-sign-and-up">
+      <SignIn />
+      <SignUp />
+    </div>
+  );
+};
 
 export default SignInAndSignUpPage;
